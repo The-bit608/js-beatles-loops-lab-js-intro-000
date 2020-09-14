d7daf9e28163694ed955b68cd302cedfea377de1
Grant Stafford-Renard <gstaffordrenard@gmail.com>
2020-09-14 17:46:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
 let facts = []
-for (let i = 0; i < musicians.length; i += 1;) {
+for (let i = 0; i < musicians.length; i += 1) {
 facts.push(`${musicians[i]} plays ${instruments[i]}.`)
 }
 return facts
