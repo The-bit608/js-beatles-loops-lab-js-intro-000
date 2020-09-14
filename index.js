296214b3e9ffecf68a5725e572937e154d8da322
Grant Stafford-Renard <gstaffordrenard@gmail.com>
2020-09-14 18:38:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function iLoveTheBeatles(number) {
 let iLoveTheBeatles = []
 do {
   iLoveTheBeatles.push('I love the Beatles!')
-} while (iLoveTheBeatles < number)
+} while (iLoveTheBeatles < number.length)
 return iLoveTheBeatles
 }
